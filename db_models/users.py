e8637a4e899776ff268e2608cebebab3a19ccd4d
Kirill <k_vasuirin@mail.ru>
2023-09-06 23:50:54 +0300

    добавлена алхимиевая модель таблицы с юзерами

---
+++ db_models/users.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    email = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
