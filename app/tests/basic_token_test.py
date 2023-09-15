5b610220da5c358e7a3d75ef8395fdcb60fed6f6
Kirill <k_vasuirin@mail.ru>
2023-09-15 17:00:22 +0300

    мне лень их переписывать

--- app/tests/basic_token_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers = {
-    'accept': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtpcmlsbF9rMWx2aW4iLCJleHAiOjE2OTQ1NDYwOTd9.8oAOdrwtzFgcqy4XM4Bs7cpTU4_4hwZpKuIE-BM_9oo',
-}
-
-response = requests.get('http://127.0.0.1:8000/users/', headers=headers)
-print(response.text)
