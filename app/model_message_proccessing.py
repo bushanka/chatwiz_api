5181b09e18b78d2d642eb14881a58b9d3fbaf764
Kirill <k_vasuirin@mail.ru>
2023-09-28 22:37:51 +0300

    hotfix

--- app/model_message_proccessing.py
+++ app/model_message_proccessing.py
@@ -10,6 +10,7 @@ import json
 async def mock_model_response(some_question: str) -> str:
     return f'The answer on "{some_question}" is : 42!'
 
+
 def convert_to_proper_chat_history(history):
     dict_history = json.loads(history)
     chat_history = dict_history['chat']
@@ -18,11 +19,12 @@ def convert_to_proper_chat_history(history):
     converted_history = [(chat_history[i][1], chat_history[i + 1][1]) for i in range(1, len(chat_history) - 1, 2)]
     return converted_history
 
-async def llm_model_response(user_question: str, message_history: dict, context_name: str) -> str:
+
+async def llm_model_response(user_question: str, message_history: str, context_name: str) -> str:
     retriever = apgvector_instance.as_retriever(name_search_collection=context_name)
     qa = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(temperature=0.3, model='gpt-3.5-turbo'), 
-        chain_type="stuff", 
+        llm=ChatOpenAI(temperature=0.3, model='gpt-3.5-turbo'),
+        chain_type="stuff",
         retriever=retriever
     )
     # FIXME: Мб лучше сразу как-то нормально подавать данные ?
@@ -40,10 +42,12 @@ async def llm_model_response(user_question: str, message_history: dict, context_
 
 
 async def add_message_to_history(new_message: Any, message_history: str) -> str:
+    if len(message_history) == 12:
+        return message_history[:-3] + "[" + json.dumps(new_message) + ']}'
     return message_history[:-2] + ", " + json.dumps(new_message) + ']}'
 
 
-async def get_new_message_history(new_message: str, message_history: dict, context_name: str) -> str:
+async def get_new_message_history(new_message: str, message_history: str, context_name: str) -> str:
     model_response = await llm_model_response(new_message, message_history, context_name)
     wrapped_new_message = ["human", new_message]
     wrapped_response = ["ai", model_response]
