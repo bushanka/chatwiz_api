5caff255acc425de3dd66a5beb34edb6fafad69b
bushanka <bushanka2805@gmail.com>
2023-10-01 12:07:37 +0000

    0.99b

--- debug.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    # import uvicorn
-    # config = uvicorn.Config("app.main:app", port=5000, log_level="info")
-    # server = uvicorn.Server(config)
-    # server.run()
-    print()
