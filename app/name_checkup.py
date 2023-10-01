9db03c5e4ea7e6b12f6784ee56300468c9a96ccf
Kirill <k_vasuirin@mail.ru>
2023-10-01 15:08:24 +0300

    добавил точку в названия файлов

--- app/name_checkup.py
+++ app/name_checkup.py
@@ -3,7 +3,10 @@ import re
 
 def check_name_safety(name: str) -> bool:
     # Паттерн для безопасных символов
-    pattern = r'^[a-zA-Z0-9\-_/\\]+$'
+    pattern = r'^[a-zA-Z0-9\-_/\\.]+$'
 
     # Проверяем, соответствует ли строка паттерну
     return bool(re.match(pattern, name))
+
+# if __name__ == '__main__':
+#     print(check_name_safety('concept.pdf'))
