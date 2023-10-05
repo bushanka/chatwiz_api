e17cffd213b98be0cfd26c8507c8c31c98249e69
Kirill <k_vasuirin@mail.ru>
2023-10-05 21:09:16 +0300

    добавлены символы для названия

--- app/name_checkup.py
+++ app/name_checkup.py
@@ -2,11 +2,12 @@ import re
 
 
 def check_name_safety(name: str) -> bool:
-    # Паттерн для безопасных символов
-    pattern = r'^[a-zA-Z0-9\-_/\\.]+$'
+    # Паттерн для безопасных символов (включая русские буквы и пробелы)
+    pattern = r'^[a-zA-Zа-яА-Я0-9()\-_/\\.\s]+$'
 
     # Проверяем, соответствует ли строка паттерну
     return bool(re.match(pattern, name))
 
-# if __name__ == '__main__':
-#     print(check_name_safety('concept_1324.pdf'))
+
+if __name__ == '__main__':
+    print(check_name_safety('KhOBOD_Spark_streaming_Kafka_amp_NoSQL_over_BigData (2).pdf'))
